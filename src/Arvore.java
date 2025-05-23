0a31d63a09d3d34c02ebceb1de6d341e78b491f7
Alan <Alan_auto_center@hotmail.com>
2025-05-22 21:24:55 -0300

    Aula 22.05 -> Implementado Busca em nível,em ordem sem recursividade e pre ordem sem recursividade

--- src/Arvore.java
+++ src/Arvore.java
@@ -1,3 +1,7 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
 public class Arvore {
     No raiz;
 
@@ -28,6 +32,74 @@ public class Arvore {
         }
     }
 
+    public void PercursoPosOrdem(No no){
+        if(no != null){
+            PercursoPosOrdem(no.esquerda);
+            PercursoPosOrdem(no.direita);
+            System.out.println(no.valor);
+        }
+    }
+
+    public void PercursoEmNivel(No no){
+        if(no != null){
+            Queue<No> fila = new LinkedList<>();
+            fila.add(no);
+
+            while (!fila.isEmpty()){
+                No atual = fila.poll();
+                System.out.println(atual.valor);
+
+                if(atual.esquerda != null){
+                    fila.add(atual.esquerda)
+                }
+                if(atual.direita != null){
+                    fila.add(atual.direita);
+                }
+            }
+        }
+    }
+
+
+    public void PreOrdemSemRecursividade(No raiz) {
+        if (raiz == null) return;
+
+        Stack<No> pilha = new Stack<>();
+        pilha.push(raiz);
+
+        while (!pilha.isEmpty()) {
+            No atual = pilha.pop();
+            System.out.println(atual.valor + " ");
+
+            if (atual.direita != null) {
+                pilha.push(atual.direita);
+            }
+
+            if (atual.esquerda != null) {
+                pilha.push(atual.esquerda);
+            }
+        }
+    }
+
+
+    public void EmOrdemSemRecursividade(No raiz) {
+        if (raiz == null) return;
+
+        Stack<No> pilha = new Stack<>();
+        pilha.push(raiz);
+
+        while (!pilha.isEmpty()) {
+            No atual = pilha.pop();
+
+            if (atual.direita != null) {
+                pilha.push(atual.direita);
+            }
+            System.out.println(atual.valor + " ");
+
+            if (atual.esquerda != null) {
+                pilha.push(atual.esquerda);
+            }
+        }
+    }
 
 }
 
